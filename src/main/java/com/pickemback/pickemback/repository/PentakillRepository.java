f1c304e08700cd38a9c72664942321c677c3dea7
FlorianHe <florian.helaine@epitech.eu>
2023-10-13 20:19:12 +0200

    Removing app-dev properties

--- src/main/java/com/pickemback/pickemback/repository/PentakillRepository.java
+++ src/main/java/com/pickemback/pickemback/repository/PentakillRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.pickemback.pickemback.entity.Pentakill;
-import com.pickemback.pickemback.entity.PentakillAmount;
 
 public interface PentakillRepository extends JpaRepository<Pentakill, Long> {
     @Query("SELECT p.player, COUNT(p) AS amount FROM Pentakill p GROUP BY p.player ORDER BY count(p) DESC")
